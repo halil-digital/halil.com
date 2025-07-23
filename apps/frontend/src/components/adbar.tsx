7aaac601079a4a1cae8d78237c13424fe11432f6
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2025-07-23 18:28:13 +0200

    fix: ad bar content

--- apps/frontend/src/components/adbar.tsx
+++ apps/frontend/src/components/adbar.tsx
@@ -4,7 +4,7 @@ export default function AdBar() {
   return (
     <div className="flex justify-center items-center gap-2 bg-[#264f36] text-white py-1 font-bold text-sm">
       <Truck />
-      Livraison en 24h 7/7j
+      Livraison en 24h 7j/7
     </div>
   );
 }
